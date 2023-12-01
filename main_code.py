d14b445ef3ef2479bcba8f20b50830f33ea013e6
Gabr1jel-01 <76105935+Gabr1jel-01@users.noreply.github.com>
2023-12-01 23:01:31 +0100

    a

--- main_code.py
+++ main_code.py
@@ -16,19 +16,28 @@ app.rowconfigure(1,weight=1)
 app.rowconfigure(2,weight=2)
 
 
+
 #VARIABLES
 
 row_counter = 1
 
+#212121 dark grey
+#3d3d3d middle grey
+#aaaaaa light grey
 
 
-#FUNCTIONS
+#181818 dark black
+
+
+#ffffff white
+
+#region FUNCTIONS
 
 def select_frame_by_name(name):
-    home_button.configure(fg_color=("#01767b", "gray25") if name == "home" else "transparent")
+    add_collection_button.configure(fg_color=("#01767b", "gray25") if name == "home" else "transparent")
 
     
-    if name == "home":
+    if name == "":
         home_view_frame.grid(row=1, column=1, sticky="nsew",rowspan=2)
     else:
             home_view_frame.grid_forget()
@@ -38,8 +47,6 @@ def select_frame_by_name(name):
 def home_button_event():
         select_frame_by_name("home")
         
-def change_appearance_mode_event(new_appearance_mode):
-    ctk.set_appearance_mode(new_appearance_mode)
 
 def add_button():
     global pop_up_window 
@@ -58,18 +65,18 @@ def add_button():
         new_collection_frame_purple.place(x=0,y=0)  
         
         new_collection_label = ctk.CTkLabel(pop_up_window,text="Please enter new collection name:",
-                                            font=("Times New Roman",24))
+                                            font=("Roboto",24))
         new_collection_label.place(x=20,y=10)
         
         new_collection_entrie = ctk.CTkEntry(pop_up_window,width=300,height=40,corner_radius=0,placeholder_text="New collection name",
                                              placeholder_text_color="grey")
         new_collection_entrie.place(x=100,y=110)
         
-        create_button = ctk.CTkButton(pop_up_window,text="Create ✓",fg_color="#85CC18",height=50,width=200,font=("Times New Roman",20),
+        create_button = ctk.CTkButton(pop_up_window,text="Create ✓",fg_color="#85CC18",height=50,width=200,font=("Roboto",20),
                                       hover_color="#56A600",command=pop_up_create_action)
         create_button.place(x=30,y=200)
         
-        create_button = ctk.CTkButton(pop_up_window,text="Cancle X",fg_color="#B51F03",height=50,width=200,font=("Times New Roman",20),
+        create_button = ctk.CTkButton(pop_up_window,text="Cancle X",fg_color="#B51F03",height=50,width=200,font=("Roboto",20),
                                       hover_color="#812100")
         create_button.place(x=270,y=200)
         
@@ -82,70 +89,113 @@ def pop_up_create_action():
     a = new_collection_entrie.get()
     global row_counter
     
-    button = ctk.CTkButton(scrollable_frame,text=a,font=("Times New Roman",24),height=80,width=200,corner_radius=0,border_width=0,
+    button = ctk.CTkButton(scrollable_frame,text=a,font=("Roboto",24),height=80,width=200,corner_radius=0,border_width=0,
                            fg_color="#53bbaf",hover_color="#01767b",text_color="black")
     button.grid(row=1+row_counter, column=0,sticky="w")
     row_counter = row_counter + 1
     pop_up_window.destroy()
     
-    
+#endregion
 
-#IMAGES
-app_main_picture = ctk.CTkImage(light_image=Image.open("png_pictures/colours.png"))
+
+
+#region IMAGES
+
 user_png_image = ctk.CTkImage(light_image=Image.open("png_pictures/user.png"))
 notification_png_image = ctk.CTkImage(light_image=Image.open("png_pictures/notification.png"))
-home_button_image = ctk.CTkImage(light_image=Image.open("png_pictures/home.png"))
+delete_collection_button = ctk.CTkImage(light_image=Image.open("png_pictures/trashcan_image.png"))
+
+#endregion
 
 
-#FRAMES
-scrollable_frame = ctk.CTkScrollableFrame(app,fg_color="white",corner_radius=0,
-                                          scrollbar_button_color="white")
-scrollable_frame.grid(row=1,column=0,sticky="ns",rowspan=2)
 
-home_view_frame = ctk.CTkFrame(app,corner_radius=0)
+#region FRAMES
+main_app_frame = ctk.CTkFrame(app,fg_color="#181818").grid(sticky="nsew",row=0,column=0,rowspan=3,columnspan=2)
+
+
+scrollable_frame = ctk.CTkScrollableFrame(main_app_frame,fg_color="#181818",corner_radius=0,
+                                          scrollbar_button_color="#181818")
+scrollable_frame.grid(row=2,column=0,sticky="ns",rowspan=2)
+
+home_view_frame = ctk.CTkFrame(main_app_frame,corner_radius=0,fg_color="#181818")
 home_view_frame.columnconfigure(2, weight=1)
 home_view_frame.rowconfigure(2, weight=1)
 
-line_under_search_bar_frame = ctk.CTkFrame(app,fg_color="#4f2d71",height=5)
-line_under_search_bar_frame.grid(row=1,column=1,sticky="new")
+main_frame_for_loaded_pictures = ctk.CTkFrame(main_app_frame,fg_color="#181818",border_color="#181818",corner_radius=0)
+main_frame_for_loaded_pictures.grid(row=1,column=1,rowspan=2,columnspan=2,sticky="nsew")
 
-#LABELS
-label1 = ctk.CTkLabel(app,corner_radius=0,image=app_main_picture,text="")
-label1.grid(row=0,column=0,sticky="")
+#endregion
 
 
-user_png_image_label = ctk.CTkLabel(app, image=user_png_image,text="",fg_color="white")
-user_png_image_label.grid(row=0,column=1,sticky="e",padx=110,ipadx=5,ipady=5)
 
-#OPTION MENUS
-user_drop_down = ctk.CTkOptionMenu(app,button_color="white",fg_color="white",dynamic_resizing=False,corner_radius=20,button_hover_color="white",
-                                   text_color="black",values=["User","Options"],font=("Times New Roman",18),dropdown_font=("Times New Roman",18))
-user_drop_down.grid(row=0,column=1,sticky="e",padx=80,ipadx=5,ipady=5)
-
-#appearance_mode_menu = ctk.CTkOptionMenu(scrollable_frame,values=["Light", "Dark", "System"],command=change_appearance_mode_event)
-#appearance_mode_menu.grid(row=0,column=0,sticky="s")
-
-#ENTRIES
-search_entry = ctk.CTkEntry(app,width=400,height=50,corner_radius=50,fg_color="white",text_color="black",placeholder_text="Search",
-                            placeholder_text_color="grey",border_color="#905d9e",font=("Times New Roman",24))
-search_entry.grid(row=0,column=1)
+#region OPTION MENUS
+user_drop_down = ctk.CTkOptionMenu(main_app_frame,button_color="#181818",fg_color="#181818",bg_color="#181818",
+                                   dynamic_resizing=False,corner_radius=20,button_hover_color="#3d3d3d",
+                                   text_color="white",values=["User","Options"],font=("Roboto",18),dropdown_font=("Roboto",18))
+user_drop_down.grid(row=0,column=1,sticky="e",padx=80,pady=10)
+#endregion
 
 
-#BUTTONS
 
-plus_button = ctk.CTkButton(app,text="+",font=("Times New Roman",36),corner_radius=40,width=1,height=40,command=add_button,
-                            fg_color="#905d9e",hover_color="#4f2d71")
-plus_button.grid(row=0,column=1,sticky="w",padx=20,pady=20,ipadx=10,ipady=10)
+#region LABELS
+user_png_image_label = ctk.CTkLabel(main_app_frame, image=user_png_image,text="",fg_color="#181818")
+user_png_image_label.grid(row=0,column=1,sticky="e",padx=110)
+
+a_project_label = ctk.CTkLabel(main_app_frame,text="AProject",font=("Roboto",32),text_color="white",fg_color="#181818")
+a_project_label.grid(row=0,column=0,sticky="nsew")
+
+#endregion
 
 
-notification_button = ctk.CTkButton(app,width=15,fg_color="#905d9e",hover_color="#4f2d71",
-                                    image=notification_png_image,text="",corner_radius=50)
-notification_button.grid(row=0,column=1,sticky="e",padx=300,ipadx=10,ipady=10)
 
-home_button = ctk.CTkButton(scrollable_frame,image=home_button_image,fg_color="#53bbaf",hover_color="#01767b",border_color="black",border_width=0,
-                            text="Home",font=("Times New Roman",24),text_color="black",height=80,width=200,corner_radius=0,command=home_button_event)
-home_button.grid(row=1,column=0,sticky="w")
+#region ENTRIES
+search_entry = ctk.CTkEntry(main_app_frame,width=400,height=50,corner_radius=50,fg_color="#181818",text_color="white",placeholder_text="Search",
+                            placeholder_text_color="white",border_color="#3d3d3d",font=("Roboto",24),bg_color="#181818")
+search_entry.grid(row=1,column=1,pady=30,sticky="n")
 
+#endregion
+
+
+
+#region BUTTONS
+
+notification_button = ctk.CTkButton(app,width=15,fg_color="#181818",hover_color="#3d3d3d",
+                                    image=notification_png_image,text="",corner_radius=50,bg_color="#181818")
+notification_button.grid(row=0,column=1,sticky="e",padx=220,ipady=10,pady=10)
+
+
+add_collection_button = ctk.CTkButton(main_app_frame,fg_color="#181818",hover_color="#3d3d3d",border_color="#181818",
+                                      border_width=0,text="+Collection",font=("Roboto",28),text_color="white",
+                                      height=80,width=200,corner_radius=0,command=home_button_event,anchor="w")
+add_collection_button.grid(row=1,column=0,sticky="w")
+
+
+delete_collection_button = ctk.CTkButton(main_app_frame,image=delete_collection_button,text="",height=80,width=30,
+                                         fg_color="#181818",hover_color="#3d3d3d",bg_color="#181818")
+delete_collection_button.grid(row=1,column=0,sticky="e")
+
+
+collections_button = ctk.CTkButton(main_app_frame,font=("Roboto",32),text="Collections",fg_color="#181818",
+                                   hover_color="#3d3d3d",corner_radius=50,bg_color="#181818")
+collections_button.grid(row=0,column=1,padx=150,sticky="w")
+
+
+import_button = ctk.CTkButton(main_app_frame,font=("Roboto",32),text="Import",fg_color="#181818",
+                              hover_color="#3d3d3d",corner_radius=50,bg_color="#181818")
+import_button.grid(row=0,column=1,sticky="w",padx=480)
+
+
+usb_button = ctk.CTkButton(main_app_frame,font=("Roboto",32),text="USB",fg_color="#181818",
+                           hover_color="#3d3d3d",corner_radius=50,bg_color="#181818")
+usb_button.grid(row=0,column=1,sticky="e",padx=690)
+
+
+settings_button = ctk.CTkButton(main_app_frame,font=("Roboto",32),text="Settings",fg_color="#181818",
+                                hover_color="#3d3d3d",corner_radius=50,bg_color="#181818")
+settings_button.grid(row=0,column=1,sticky="e",padx=400)
+
+
+#endregion
 
 
 
